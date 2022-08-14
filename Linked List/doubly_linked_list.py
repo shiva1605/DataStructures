347512bc79c6a677a0819dbba9ec11d64d362f15
Shiva Kumar <kumar.shiva2012@gmail.com>
2022-08-14 22:18:36 +0530

    Added Doubly Linked List Code

--- Linked List/doubly_linked_list.py
+++ Linked List/doubly_linked_list.py
@@ -1,6 +1,7 @@
 class Node:
     def __init__(self, data):
         self.__data = data
+        self.__prev = None
         self.__next = None
 
 
@@ -20,7 +21,15 @@ class Node:
         self.__next = next_node
 
 
-class LinkedList:
+    def get_prev(self):
+        return self.__prev
+
+
+    def set_prev(self, prev_node):
+        self.__prev = prev_node
+
+
+class DoublyLinkedList:
     def __init__(self):
         self.__head = None
         self.__tail = None
@@ -53,6 +62,7 @@ class LinkedList:
             temp1 = temp.get_next()
             temp.set_data(None)
             temp.set_next(None)
+            temp.set_prev(None)
             temp = temp1
         self.__head = None
         self.__tail = None
@@ -65,20 +75,22 @@ class LinkedList:
 
     def __add_first(self, data):
         new_node = Node(data)
-        new_node.set_next(self.__head)
-        self.__head = new_node
         if(self.is_empty()):#If Linked List is empty
-            self.__tail = new_node
+            self.__head = self.__tail = new_node
+        else:
+            new_node.set_next(self.__head)
+            self.__head.set_prev(new_node)
+            self.__head = new_node
         self.__size += 1
 
 
     def __add_last(self, data):
         new_node = Node(data)
         if(self.is_empty()):
-            self.__head = new_node
-            self.__tail = new_node
+            self.__head = self.__tail = new_node
         else:
             self.__tail.set_next(new_node)
+            new_node.set_prev(self.__tail)
             self.__tail = new_node
         self.__size += 1
 
@@ -97,6 +109,8 @@ class LinkedList:
             self.__head = self.__head.get_next()
             if(self.__head == None):#If Linked List becomes empty after removing this element
                 self.__tail = None
+            else:
+                self.__head.set_prev(None)
             self.__size -= 1
             return node.get_data()
 
@@ -106,17 +120,12 @@ class LinkedList:
             print('Linked List Empty!')
             return
         else:
-            temp = self.__head
-            temp1 = None
             data = self.__tail.get_data()
-            while(temp.get_next() != None):# Find the node previous to the tail node
-                temp1 = temp
-                temp = temp.get_next()
-            temp1.set_next(None)
-            if(self.__size==1):
-                self.__tail, self.__head = None, None
+            self.__tail = self.__tail.get_prev()
+            if(self.__tail == None):#If Linked List becomes empty after removing this element
+                self.__head = None
             else:
-                self.__tail = temp1
+                self.__tail.set_next(None)
             self.__size -= 1
             return data
 
@@ -124,19 +133,19 @@ class LinkedList:
     def insert(self, data, index): #inserts the data at a certain index position. This method returns None
         if(index<0 or index>self.__size):
             print('Invalid Index')
-        elif(index==0):
+        elif(index==0): #Insertion at the beginning
             self.__add_first(data)
-        elif(index==self.__size):
+        elif(index==self.__size): #Insertion at the end
             self.__add_last(data)
-        else:
+        else: #Insertion in the middle
             new_node = Node(data)
-            temp = self.__head
-            temp1 = None
+            temp = self.__head 
             for i in range(0, index):
-                temp1 = temp
                 temp = temp.get_next()
             new_node.set_next(temp)
-            temp1.set_next(new_node)
+            new_node.set_prev(temp.get_prev())
+            temp.get_prev().set_next(new_node)
+            temp.set_prev(new_node)
             self.__size += 1
 
 
@@ -151,24 +160,23 @@ class LinkedList:
             return self.__remove_last()
         else:
             temp = self.__head
-            temp1 = None
             for i in range(0, index):
-                temp1 = temp
                 temp = temp.get_next()
             data = temp.get_data()
-            temp1.set_next(temp.get_next())
+            temp.get_prev().set_next(temp.get_next())
+            temp.get_next().set_prev(temp.get_prev())
             '''
-            The next two lines of the code are for making the data and the next part of the node to be removed as None.
+            The next three lines of the code are for making the data and the next part of the node to be removed as None.
             This is not needed in Python since the temp node will not have any reference once this  function is completed.
             Since temp is a local variable and the scope of temp is only within the function
             In Python, The object which does not have any reference is automatically eligible for garbage collection
             '''
             temp.set_data(None)
             temp.set_next(None)
+            temp.set_prev(None)
             return data
 
-
-    def display(self):
+    def displayForward(self):
         if(self.__size==0):
             print('Linked List is empty')
             return
@@ -178,21 +186,39 @@ class LinkedList:
             temp = temp.get_next()
 
 
+    def displayReverse(self):
+        if(self.__size==0):
+            print('Linked List is empty')
+            return
+        temp = self.__tail
+        while(temp != None):
+            print(temp.get_data())
+            temp = temp.get_prev()
 
-my_linked_list = LinkedList()
+
+my_linked_list = DoublyLinkedList()
 my_linked_list.add(1)
 my_linked_list.add(2)
 my_linked_list.add(3)
-my_linked_list.display()
+print('Diplaying Elements Forward')
+my_linked_list.displayForward()
+print('Diplaying Elements In Reverse')
+my_linked_list.displayReverse()
 my_linked_list.insert(4, 0)
 print('===After Inserting===')
-print('Diplaying Elements')
-my_linked_list.display()
+print('Diplaying Elements Forward')
+my_linked_list.displayForward()
+print('Diplaying Elements In Reverse')
+my_linked_list.displayReverse()
 my_linked_list.insert(5, 0)
 print('===After Inserting===')
-print('Diplaying Elements')
-my_linked_list.display()
+print('Diplaying Elements Forward')
+my_linked_list.displayForward()
+print('Diplaying Elements In Reverse')
+my_linked_list.displayReverse()
 my_linked_list.remove(2)
 print('===After Removal===')
-print('Diplaying Elements')
-my_linked_list.display()
+print('Diplaying Elements Forward')
+my_linked_list.displayForward()
+print('Diplaying Elements In Reverse')
+my_linked_list.displayReverse()
